d0833c76ec16ec8204ac19e91176c3749293f1cf
Kyle Huynh <kyle.huynh205@gmail.com>
2021-12-13 20:10:03 +0000

    Sync taxonomy term with Group

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -1,70 +1,81 @@
 <?php
 
-  namespace Drupal\group_access_control\Form;
+namespace Drupal\group_access_control\Form;
 
-  use Drupal\Core\Form\ConfigFormBase;
-  use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class ConfigForm.
+ */
+class ConfigForm extends ConfigFormBase {
+  const CONFIG_NAME = 'group_access_control.config';
+  const CONFIG_TAXONOMY_VOCAL = 'group_access_control.taxonomy.vocabulary';
 
   /**
-   * Class ConfigForm.
+   * {@inheritdoc}
    */
-  class ConfigForm extends ConfigFormBase
-  {
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function getEditableConfigNames()
-    {
-      return [
-        'group_access_control.config',
-      ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getFormId()
-    {
-      return 'config_form';
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(array $form, FormStateInterface $form_state)
-    {
-      $config = $this->config('group_access_control.config');
-      $form = parent::buildForm($form, $form_state);
-
-      $entity = \Drupal::entityTypeManager()->getStorage('taxonomy_vocabulary');
-      $query = $entity->getQuery();
-      $taxonomy_ids = $query->execute();
-      $form['taxonomy-vocabulary'] = [
-        '#type' => 'select',
-        '#name' => 'queues',
-        '#title' => $this->t('Select a Taxonomy Vocabulary to associate with Group:'),
-        '#required' => TRUE,
-        '#options' => $taxonomy_ids,
-      ];
-
-      // TODO: Select an toxnomomy vocal, in hook check the caught entity's reference fiedls if any associate with selected vocal, proceed, with Danny hoook
-
-      // TODD: Catch Group created, use this asocated taxno
-
-
-      return $form;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state)
-    {
-      parent::submitForm($form, $form_state);
-
-      $this->config('group_access_control.config')
-        ->save();
-    }
-
+  protected function getEditableConfigNames() {
+    return [
+      self::CONFIG_NAME,
+    ];
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'config_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config(self::CONFIG_NAME);
+
+    // get list of existed groups
+    $group_types = \Drupal::service('entity_type.manager')->getStorage('group_type')->loadMultiple();
+
+    // get list existing taxonomy vocabulary
+    $entity = \Drupal::entityTypeManager()->getStorage('taxonomy_vocabulary');
+    $query = $entity->getQuery();
+    $taxonomy_ids = $query->execute();
+
+    $form['description'] = [
+      '#markup' => $this->t("<p>Select a Taxonomy Vocabulary to associate with a Group Type for: </p><ul><li>When a group of that Group Type is created, a term in that vocabulary with the same name as Group name</li></ul>"),
+    ];
+
+    foreach ($group_types as $group_type) {
+      $form[$group_type->id()] = [
+        '#type' => 'select',
+        '#name' => $group_type->id(),
+        '#title' => $this->t('For <i>' . $group_type->label() . "</i> group:"),
+        '#options' => $taxonomy_ids,
+        '#required' => true,
+        '#default_value' => $config->get($group_type->id(), 0)
+      ];
+    }
+
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->configFactory->getEditable(self::CONFIG_NAME);
+
+    $groups = \Drupal::service('entity_type.manager')->getStorage('group_type')->loadMultiple();
+    foreach ($groups as $group) {
+      if ($form_state->getValues()[$group->id()] !== NULL) {
+        $config->set($group->id(), $form_state->getValues()[$group->id()])->save();
+      }
+    }
+
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
